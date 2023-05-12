f136940c2069def739f3de620fb8e5e9dcb9e512
valeriam3100 <mon18014@byui.edu>
2023-05-12 23:08:18 +0100

    done

---
+++ chamber/scipts.js
@@ -0,0 +1,10 @@
+
+const hambugerb = document.querySelector('#hamburgerb');
+const primary = document.querySelector('.primary');
+
+hamburgerb.addEventListener('click', () => {
+	navigation.classList.toggle('responsive');
+});
+
+         
+        
