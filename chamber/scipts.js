db019f86871ab350794a05038305bb99f901b797
valeriam3100 <mon18014@byui.edu>
2023-05-15 23:36:30 +0100

    done

--- chamber/scipts.js
+++ chamber/scipts.js
@@ -1,10 +1,11 @@
+function togglemenu() {
+	document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
+  }
+  const now = new Date();
+  const nowDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full"
+  }).format(now);
+  document.getElementById('header-date')
+    .innerText = nowDate; 
 
-const hambugerb = document.querySelector('#hamburgerb');
-const primary = document.querySelector('.primary');
-
-hamburgerb.addEventListener('click', () => {
-	navigation.classList.toggle('responsive');
-});
-
-         
-        
+    
